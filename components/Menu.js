3c94513871afafb998ad3b4079459d8d4bb3479b
burn-Thor <eothorburn@gmail.com>
2022-09-08 10:09:30 +0100

    pizza page start

--- components/Menu.js
+++ components/Menu.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Menu.module.css'
 import Image from 'next/image'
 import { urlFor } from '../lib/client'
+import Link from 'next/link'
 
 export default function Menu({pizzas}) {
     console.log(pizzas)
@@ -19,13 +20,16 @@ export default function Menu({pizzas}) {
                 
                 return(
                     <div className={styles.pizza} key={id}>
+                        <Link href={`./pizza/${pizza.slug.current}`}>
                         <div className={styles.ImageWrapper}>
                             <Image 
                             loader = {()=>src}
                             src={src} alt='' objectFit='cover' layout='fill'/>
                         </div>
+                        </Link>
                         <span>{pizza.name}</span>
                         <span><span style={{color:'var(--themeRed)'}}>£</span>{pizza.price[0]}</span>
+                        
                     </div>
                 )
             })}
---
+++ pages/pizza/[slug].js
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+
+export default function Pizza ({pizza}) {
+    return(
+        <Layout>
+            Pizza Page
+        </Layout>
+    )
+    
+};
